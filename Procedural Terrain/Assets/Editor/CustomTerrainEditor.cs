589db999cf57da5b773d3d661dcb2778005fa07b
Noah Day <sheltielover432@gmail.com>
2020-11-13 23:24:19 -0500

    Generating Random Heights

--- Procedural Terrain/Assets/Editor/CustomTerrainEditor.cs
+++ Procedural Terrain/Assets/Editor/CustomTerrainEditor.cs
@@ -11,14 +11,44 @@ using UnityEditor.Experimental.TerrainAPI;
 
 public class CustomTerrainEditor : Editor
 {
+    //Properties
+    SerializedProperty randomHightRange;
+    
+    //Fold outs
+    bool showRandom = false;
+    
     private void OnEnable()
     {
-        throw new NotImplementedException();
+        randomHightRange = serializedObject.FindProperty("randomHeightRange");
     }
 
     public override void OnInspectorGUI()
     {
-        
+        //Start
+        serializedObject.Update();
+        //Start
+
+        CustomTerrain terrain = (CustomTerrain) target;
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random");
+        if (showRandom)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(randomHightRange);
+            if (GUILayout.Button("Random Heights"))
+            {
+                terrain.RandomTerrain();
+            }
+        }
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (GUILayout.Button("Reset Terrain"))
+            {
+                terrain.ResetTerrain();
+            }
+
+        //End
+        serializedObject.ApplyModifiedProperties();
+        //End
     }
 
     // Start is called before the first frame update
--- Procedural Terrain/Assets/Terrain Assets/Scripts/CustomTerrain.cs
+++ Procedural Terrain/Assets/Terrain Assets/Scripts/CustomTerrain.cs
@@ -9,6 +9,44 @@ using System.Linq;
 
 public class CustomTerrain : MonoBehaviour
 {
+    public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+    public Terrain terrain;
+    public TerrainData terrainData;
+    
+    public void RandomTerrain()
+    {
+        float[,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
+        for (int x = 0; x < terrainData.heightmapResolution; x++)
+        {
+            for (int z = 0; z < terrainData.heightmapResolution; z++)
+            {
+                heightMap[x, z] += UnityEngine.Random.Range(randomHeightRange.x, randomHeightRange.y);
+            }
+            terrainData.SetHeights(0,0, heightMap);
+        }
+    }
+    
+    public void ResetTerrain()
+    {
+        float[,] heightMap;
+            heightMap = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
+        for (int x = 0; x < terrainData.heightmapResolution; x++)
+        {
+            for (int z = 0; z < terrainData.heightmapResolution; z++)
+            {
+                heightMap[x, z] = 0;
+            }
+            terrainData.SetHeights(0,0, heightMap);
+        }
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Initializing Terrain Data");
+        terrain = this.GetComponent<Terrain>();
+        terrainData = Terrain.activeTerrain.terrainData;
+    }
+
     private void Awake()
     {
         SerializedObject tagManager = new SerializedObject(
@@ -22,24 +60,19 @@ public class CustomTerrain : MonoBehaviour
         this.gameObject.tag = "Terrain";
     }
 
-    private void AddTag(SerializedProperty tagsProp, string newTag)
+    void AddTag(SerializedProperty tagsProp, string newTag)
     {
         bool found = false;
         for (int i = 0; i < tagsProp.arraySize; i++)
         {
             SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
-            if (t.stringValue.Equals(newTag))
-            {
-                found = true;
-                break;
-            }
-
-            if (!found)
-            {
-                tagsProp.InsertArrayElementAtIndex(0);
-                SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
-                newTagProp.stringValue = newTag;
-            }
+            if (t.stringValue.Equals(newTag)) { found = true; break; }
+        }
+        if (!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
         }
     }
 }
