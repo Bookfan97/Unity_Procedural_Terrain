263fe72350940abb769d047a6035ed48d0d0408d
Noah Day <sheltielover432@gmail.com>
2020-11-13 22:36:31 -0500

    Getting Ready to Create a Custom Editor

---
+++ Procedural Terrain/Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+using UnityEditor.Experimental.TerrainAPI;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+
+public class CustomTerrainEditor : Editor
+{
+    private void OnEnable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Procedural Terrain/Assets/Terrain Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour
+{
+    private void Awake()
+    {
+        SerializedObject tagManager = new SerializedObject(
+            AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]
+            );
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+        tagManager.ApplyModifiedProperties();
+        this.gameObject.tag = "Terrain";
+    }
+
+    private void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+        for (int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if (t.stringValue.Equals(newTag))
+            {
+                found = true;
+                break;
+            }
+
+            if (!found)
+            {
+                tagsProp.InsertArrayElementAtIndex(0);
+                SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+                newTagProp.stringValue = newTag;
+            }
+        }
+    }
+}
