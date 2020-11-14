61ad6d2c2a527276302f9e4d8fadfe5743922d8d
Noah Day <sheltielover432@gmail.com>
2020-11-14 17:46:55 -0500

    Simple Perlin Noise

--- Procedural Terrain/Assets/Editor/CustomTerrainEditor.cs
+++ Procedural Terrain/Assets/Editor/CustomTerrainEditor.cs
@@ -15,16 +15,25 @@ public class CustomTerrainEditor : Editor
     SerializedProperty randomHightRange;
     SerializedProperty heightMapScale;
     SerializedProperty heightMapImage;
+    SerializedProperty perlinXScale;
+    SerializedProperty perlinYScale;
+    SerializedProperty perlinOffsetX;
+    SerializedProperty perlinOffsetY;
     
     //Fold outs
     bool showRandom = false;
     bool showLoadHeights = false;
+    bool showPerlinNoise = false;
     
     private void OnEnable()
     {
         randomHightRange = serializedObject.FindProperty("randomHeightRange");
         heightMapImage = serializedObject.FindProperty("heightMapScale");
         heightMapScale = serializedObject.FindProperty("heightMapImage");
+        perlinXScale = serializedObject.FindProperty("perlinXScale");
+        perlinYScale = serializedObject.FindProperty("perlinYScale");
+        perlinOffsetX = serializedObject.FindProperty("perlinOffsetX");
+        perlinOffsetY = serializedObject.FindProperty("perlinOffsetY");
     }
 
     public override void OnInspectorGUI()
@@ -34,7 +43,7 @@ public class CustomTerrainEditor : Editor
         //Start
 
         CustomTerrain terrain = (CustomTerrain) target;
-        showRandom = EditorGUILayout.Foldout(showRandom, "Random");
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random Heights");
         if (showRandom)
         {
             EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
@@ -56,6 +65,21 @@ public class CustomTerrainEditor : Editor
             if (GUILayout.Button("Load Texture"))
             {
                 terrain.LoadTexture();
+            }
+        }
+        
+        showLoadHeights = EditorGUILayout.Foldout(showLoadHeights, "Single Perlin Noise");
+        if (showLoadHeights)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Perlin Noise Scale", EditorStyles.boldLabel);
+            EditorGUILayout.Slider(perlinXScale, 0, 1, new GUIContent("X Scale"));
+            EditorGUILayout.Slider(perlinYScale, 0, 1, new GUIContent("Y Scale"));
+            EditorGUILayout.IntSlider(perlinOffsetX, 0, 1000, new GUIContent("Offset X"));
+            EditorGUILayout.IntSlider(perlinOffsetY, 0, 1000, new GUIContent("Offset Y"));
+            if (GUILayout.Button("Run Perlin Noise"))
+            {
+                terrain.Perlin();
             }
         }
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
--- Procedural Terrain/Assets/Scripts/CustomTerrain.cs
+++ Procedural Terrain/Assets/Scripts/CustomTerrain.cs
@@ -12,11 +12,33 @@ public class CustomTerrain : MonoBehaviour
     public Vector2 randomHeightRange = new Vector2(0, 0.1f);
     public Texture2D heightMapImage;
     public Vector3 heightMapScale = new Vector3(1,1,1); 
-        
+    
+    //Perlin Noise
+    public float perlinXScale = 0.01f;
+    public float perlinYScale = 0.01f;
+    public int perlinOffsetX = 0;
+    public int perlinOffsetY = 0;
     public Terrain terrain;
     public TerrainData terrainData;
-    
 
+    public void Perlin()
+    {
+        float[,] heightMap = terrainData.GetHeights(
+            0,
+            0,
+            terrainData.heightmapResolution,
+            terrainData.heightmapResolution
+            );
+        for (int y = 0; y < terrainData.heightmapResolution; y++)
+        {
+            for (int x = 0; x < terrainData.heightmapResolution; x++)
+            {
+                heightMap[x, y] = Mathf.PerlinNoise((x + perlinOffsetX) * perlinXScale, 
+                    (y + perlinOffsetY) * perlinYScale);
+            }
+        }
+        terrainData.SetHeights(0,0, heightMap);
+    }
     public void RandomTerrain()
     {
         float[,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
